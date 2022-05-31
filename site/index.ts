d99d34f7c8fe66c90be1b1c45cb1d681dcbcd78e
bcherry4 <bcherry4@ford.com>
2022-05-31 14:42:09 -0600

    Add ts files from prior project

---
+++ site/index.ts
@@ -0,0 +1,70 @@
+const url = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=91"
+const main = document.querySelector<HTMLBodyElement>("main")
+const pokeBox = document.querySelector<HTMLDivElement>(".pokeBox")
+const ul = document.querySelector<HTMLUListElement>("ul")
+
+
+
+type PokemonData = {
+    name: string;
+    imageUrl: string;
+}
+
+type PokemonResponse = {
+
+    data?: {
+        pokemon: PokemonData
+    }
+    results: [{
+        name: string
+        url: string
+    }]
+    errors?: { message: string }[]
+}
+
+fetch(url)
+    .then((response) => response.json())
+    .then((response: PokemonResponse) => {
+        const pokeBall = response.results
+        const httpReq = pokeBall
+            .map(pokemon => pokemon.url)
+            .map(url => {
+                return fetch(url).then(response => response.json())
+            })
+        return Promise.all(httpReq)
+        console.log(response)
+        // Safe
+    }).then(responses => {
+        responses.map(
+            response => {
+                const pokemonList = document.createElement("div")
+                const name = `${response.species.name[0].toUpperCase()}${response.species.name.slice(1)}`;
+                pokemonList.innerHTML = `
+                 <figure>
+                    <img src ="${response.sprites.front_shiny}" alt="${name}" />
+                    <figcaption>
+                        <a href="pokemon.html?pokemon=${response.name}">
+                        ${name}
+                        </a>
+                    </figcaption
+                 </figure>
+                `
+                return pokemonList
+            }
+        ).forEach(pokemonList => {
+            console.log("run Forest")
+            ul.append(pokemonList)
+        })
+    })
+    .catch((error) => {
+        const message = (error instanceof Error)
+            ? error.message
+            : "Unknown error"
+        console.error(message)
+    })
+
+
+
+
+
+export default {}
---
+++ site/pokemon.ts
@@ -0,0 +1,68 @@
+const ul = document.querySelector<HTMLUListElement>(".pokemon")
+const queryString = new URLSearchParams(window.location.search)
+
+
+function addPokemonImage(response: PokemonEntry) {
+    const div = document.querySelector<HTMLDivElement>(".abilitiesBox")
+    const pokeTitleCase = `${response.name[0].toUpperCase()}${response.name.slice(1)}`
+    div.innerHTML = `
+        <figure>
+            <img src="${response.sprites.front_shiny}" alt="${pokeTitleCase}" />
+            <figcaption>${pokeTitleCase}</figcaption>
+            <p>Abilities: ${response.abilities[0].ability.name}</p>
+        </figure>
+    `
+    addPokemonAbility(response),
+    console.log(response.abilities[0].ability.name)
+    ul.append(div)
+}
+
+function addPokemonAbility(response: PokemonEntry) {
+    const li = document.createElement("li")
+    li.innerHTML = ` 
+    <span class="ability-name">${response.abilities[0].ability.name} - </span>
+    <span class="ability-short-description">${response.abilities[0].ability.url}</span>
+`
+    ul.append(li)
+
+}
+
+
+type PokemonEntry = {
+    sprites: { front_shiny: string };
+    name: string;
+    abilities: [{ ability: {
+        name: string;
+        url: string;
+            }}] 
+}
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${queryString.get("pokemon")}`)
+    .then((response) => response.json())
+    .then((response: PokemonEntry) => {
+        const abilitiesRequests = response.abilities
+            .map(ability => ability.ability)
+            .map(urlInd => {
+                return fetch(urlInd).then(response => response.json())
+            })
+        addPokemonImage(response)
+        return Promise.all(abilitiesRequests)
+    }).then(response => {
+        response.map(response => {
+            const li = document.createElement("li")
+            li.innerHTML = `
+            <span class="ability-name">
+            ${response.name[0].toUpperCase()}${response.name.slice(1)}
+        </span>
+        <span class="ability-short-description">
+            ${response.abilities.ability}
+        </span>
+        `
+            return li
+        }).forEach(li => {
+            ul.append(li)
+        })
+    })
+
+
+export default {}
